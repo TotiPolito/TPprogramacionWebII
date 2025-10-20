2b76640b4f6031568f645148053406ce2bad0059
Roocasa <casanovarocio5@gmail.com>
2025-10-20 17:33:50 -0300

    Se elimino un include ya que no se utilizaba

--- helper/ConfigFactory.php
+++ helper/ConfigFactory.php
@@ -1,6 +1,5 @@
 <?php
 include_once(__DIR__ . "/MyConexion.php");
-include_once(__DIR__ . "/IncludeFileRenderer.php");
 include_once(__DIR__ . "/NewRouter.php");
 include_once(__DIR__ . "/MustacheRenderer.php");
 
