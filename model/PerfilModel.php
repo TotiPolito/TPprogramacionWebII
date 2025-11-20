d4bd606a056d0030244a7e77e8949731a2e7cae6
Rocio Lujan Casanova <casanovarocio5@gmail.com>
2025-11-20 11:45:39 -0300

    Se agrego el editar perfil en el home

--- model/PerfilModel.php
+++ model/PerfilModel.php
@@ -20,46 +20,5 @@ class PerfilModel {
         $stmt->close();
         return $usuario;
     }
-
-    public function editarPerfil($id, $usuario, $password, $fotoPerfil)
-    {
-        $sql = "UPDATE usuarios SET ";
-        $paramTypes = "";
-        $params = [];
-
-        if (!empty($usuario)) {
-            $sql .= "usuario=?, ";
-            $paramTypes .= "s";
-            $params[] = $usuario;
-        }
-
-        if (!empty($password)) {
-            $sql .= "password=?, ";
-            $paramTypes .= "s";
-            $params[] = password_hash($password, PASSWORD_DEFAULT);
-        }
-
-        if (!empty($fotoPerfil)) {
-            $sql .= "foto_perfil=?, ";
-            $paramTypes .= "s";
-            $params[] = $fotoPerfil;
-        }
-
-        if (empty($params)) {
-            return false;
-        }
-
-        $sql = rtrim($sql, ", ");
-
-        $sql .= " WHERE id=?";
-        $paramTypes .= "i";
-        $params[] = $id;
-
-        $stmt = $this->conexion->prepare($sql);
-        $stmt->bind_param($paramTypes, ...$params);
-
-        return $stmt->execute();
-    }
-
 }
 
